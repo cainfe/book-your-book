3f628c846af1b9581edec027f1581be62e1b044a
Floyd <Floydcain2@gmail.com>
2021-11-03 21:40:27 -0500

    finished login page
    Got login page working and succesfully connecting to database, verifying username and password, and beginning user session.

---
+++ scripts/login.php
@@ -0,0 +1,37 @@
+<?php
+    // Create variables
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $user = 0;
+    
+    // Connect to the database
+    $dbConn = new PDO('sqlite:../Data.db');
+
+    $result = $dbConn->query("SELECT username, password, userID FROM UserAccount");
+
+    // Check to see if username and password correspond
+    foreach($result as $row) {
+        if ($row['username'] == $username) {
+            if ($row['password'] == $password) {
+                // if so, begin session and store user variables
+                $user = $row['userID'];
+                session_start();
+                $_SESSION["userID"] = "$user";
+                $_SESSION["username"] = "$username";
+            }
+        }
+    }
+
+    if ($user == 0) {
+        // Close database connection and return to login screen for failed
+        // attempt
+        $dbConn = null;
+        header('Location: ../pages/login.html');
+    }
+    else {
+        // Close database connection and go on to homepage for successful 
+        // login
+        $dbConn = null;
+        header('Location: ../index.html');
+    }
+?>
