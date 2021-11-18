b622287d9419d899f0a9e10131917f47e923779a
Floyd <Floydcain2@gmail.com>
2021-11-18 10:51:56 -0600

    Got Add Book from completed

--- index.php
+++ index.php
@@ -25,17 +25,62 @@
         ?>
 
         <!-- popup form - add book -->
-        <div class="popup-form container form-container" id="add-book">
+        <div class="opup-form container form-container" id="add-book">
 			<button class="form-close-btn" onclick="toggleForm('add-book', 0)">x</button>
             <h2 class="form-title">ADD BOOK</form></h2>
 			<form id="login-form" action="../scripts/addBook.php" method="post">
+				<div class="form-data">
+					<label>ISBN</label>
+					<input type="text" name="isbn" class="data-input" id="add-book-isbn-field" required>
+				</div>
 				<div class="form-data">
 					<label>Title</label>
 					<input type="text" name="title" class="data-input" id="add-book-title-field" required>
 				</div>
 				<div class="form-data">
-					<label></label>
-					<input type="text" name="" class="data-input" id="add-book--field" required>
+					<label>Author</label><br>
+					<select type="text" name="author" class="data-input" id="add-book-author-field" required>
+                        <option disabled selected value></option>
+                        <?php
+                        $dbConn = new PDO('sqlite:Data.db');
+                        $result = $dbConn->query("SELECT authorID, fName, lName FROM Authors");
+
+                        foreach($result as $row) {
+                            $authorID = $row['authorID'];
+                            $fName = $row['fName'];
+                            $lName = $row['lName'];
+                            echo("<option value=\"$authorID\">$fName $lName</option>");
+                        }
+                        ?>
+                    </select>
+				</div>
+				<div class="form-data">
+					<label>Publisher</label><br>
+					<select type="text" name="publisher" class="data-input" id="add-book-publisher-field" required>
+                        <option disabled selected value></option>
+                        <?php
+                        $dbConn = new PDO('sqlite:Data.db');
+                        $result = $dbConn->query("SELECT supplierID, name FROM Suppliers");
+
+                        foreach($result as $row) {
+                            $supplierID = $row['supplierID'];
+                            $name = $row['name'];
+                            echo("<option value=\"$authorID\">$name</option>");
+                        }
+                        ?>
+                    </select>
+				</div>
+				<div class="form-data">
+					<label>Publication Date</label>
+					<input type="date" name="publication-date" class="data-input" id="add-book-publicaton-date-field" required>
+				</div>
+				<div class="form-data">
+					<label>Price</label>
+					<input type="number" step="any" name="price" class="data-input" id="add-book-price-field" required>
+				</div>
+				<div class="form-data">
+					<label>Reviews</label>
+					<input type="number" min="0" max="5" step="any" name="reviews" class="data-input" id="add-book-reviews-field" required>
 				</div>
 
 				<button type="submit" id="btn-submit-book" class="form-submit-btn btn-submit-book">Add</button>
