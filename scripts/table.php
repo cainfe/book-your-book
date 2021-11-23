ec158f016bf9d8de61f1b4730edb8ba5f896c62f
Alexis Morris <alexis417@icloud.com>
2021-11-22 18:21:03 -0600

    Working on table

--- scripts/table.php
+++ scripts/table.php
@@ -3,13 +3,16 @@
     $dbConn = new PDO('sqlite:../Data.db');
 
     //Need to add category to query
-    $result = $dbConn->query("SELECT * FROM Books");
-
+    /* $result = $dbConn->query("SELECT title, publicationDate, description, reviews 
+                            FROM Books, AssignedCategory
+                            WHERE Books.ISBN = AssignedCategory.bookID AND AssignedCategory.categoryCode = BookCategories.code
+                            INNER JOIN AssignedCategory ON ");
+ */
     while($row = $result->fetch_assoc())
     {
         $title = $row['title']
         $publication = $row['publicationDate']
-        $category = //$row['description']
+        $category = $row['description']
         $review = $row['reviews']
     }
 ?>
