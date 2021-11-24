3a217cbbd7fcc7306fd48bc66235c35b1f5a723c
KyleNagle <Kylenagle6@gmail.com>
2021-11-24 11:25:57 -0600

    Update addSupplier.php
    Also added code

--- scripts/addSupplier.php
+++ scripts/addSupplier.php
@@ -1,6 +1,9 @@
 <?php
-    $name = $_POST['name'];
+     $name = $_POST['name'];
 
     // Connect to the database
     $dbConn = new PDO('sqlite:../Data.db');
+
+     $result = $dbConn->querry("INSERT INTO suppliers(name) VALUES('$name');");
+
 ?>
