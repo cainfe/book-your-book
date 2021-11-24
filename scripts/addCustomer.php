00cad4019af27c27cb42da5aa135818b078f4fb9
KyleNagle <Kylenagle6@gmail.com>
2021-11-24 11:26:07 -0600

    Update addCustomer.php
    added code?

--- scripts/addCustomer.php
+++ scripts/addCustomer.php
@@ -1,13 +1,39 @@
 <?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
+     $fname = $_POST['first-name'];
+    $lname = $_POST['last-name'];
+    $gender = $_POST['gender'];
     $birthday = $_POST['birthday'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $isAdmin = $_POST['isAdmin'];
+
+    echo("$fname, $lname, $gender, $birthday, $email, $phone, $address");
 
     // Connect to the database
     $dbConn = new PDO('sqlite:../Data.db');
+
+    $contactID = $dbConn->querry("SELECT MAX(contactID) FROM ContactDetails");
+    if($contactID == null){
+        $contactID = 1;
+    }
+    else {
+
+    $contactID++;
+    }
+
+    $dbConn->querry("INSERT INTO ContactDetails(ContactID) VALUES($contactID);");
+
+    $result = $dbConn->querry("INSERT INTO Authors(fName, lName, gender, birthDate, contactID)
+VALUES('$fname','$lname', '$gender', '$birthday', $contactID)");
+
+
+        $dbConn->querry("INSERT INTO ContactEmail (contactID, email) VALUES($contactID, '$email');");
+        $dbConn->querry("INSERT INTO ContactPhone ( contactID, phone) VALUES($contactID, $phone);");
+
+
+
+
 ?>
