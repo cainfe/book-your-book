db68b41ba4e3c162ef30c230053d96d3f2d0d784
Floyd <Floydcain2@gmail.com>
2021-11-24 22:10:17 -0600

    Got addBook working

--- scripts/addBook.php
+++ scripts/addBook.php
@@ -1,8 +1,8 @@
 <?php
     $isbn = $_POST['isbn'];
     $title = $_POST['title'];
-    $author = $_POST['author'];                     // Returns the ID of the Author
-    $supplier = $_POST['supplier'];               // Returns the ID of the publisher
+    $authorID = $_POST['author'];                     // Returns the ID of the Author
+    $supplierID = $_POST['supplier'];                   // Returns the ID of the publisher
     $publicationDate = $_POST['publication-date'];
     $price = $_POST['price'];
     $reviews = $_POST['reviews'];
@@ -12,9 +12,10 @@
 
     //Check if we need $result here when site is working. Also check if we need % before and after variables.
     $result = $dbConn->query("INSERT INTO Books(ISBN, title, publicationDate, price, suppliedBy, reviews) 
-    VALUES($isbn, $title, $publicationDate, $price, $supplier, $reviews;");
+                            VALUES($isbn, '$title', '$publicationDate', $price, $supplierID, $reviews);");
 
-    $authorID = $dbConn->query("SELECT authorID FROM Authors WHERE fName = %$author%; ");
-
-    $result = $dbConn->query("INSERT INTO BookAuthors(authorID, bookID) VALUES(%$authorID%,%$isbn%)");
+    $result = $dbConn->query("INSERT INTO BookAuthors(authorID, bookID) VALUES($authorID, $isbn);");
+    
+    // Return to admin page.
+    header('Location: ../pages/AdminFE.php');
 ?>
