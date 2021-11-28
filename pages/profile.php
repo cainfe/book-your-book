538beb8c7b79dc359987e485d9e273f8b7d46695
Floyd <Floydcain2@gmail.com>
2021-11-27 18:04:50 -0600

    Update profile.php

--- pages/profile.php
+++ pages/profile.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css2?family=Gentium+Book+Basic&display=swap" rel="stylesheet">
         <link rel = "stylesheet"  href="../styles/style.css">
-        <title>Book your Book</title>
+        <title>Book your Book | Profile</title>
     </head>
 
     <body>
