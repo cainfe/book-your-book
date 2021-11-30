a5ecfda59539c34a0f3d72eecbedf82af70f4acb
Floyd <Floydcain2@gmail.com>
2021-11-30 01:14:42 -0600

    Added data to Data.db

--- scripts/addCustomer.php
+++ scripts/addCustomer.php
@@ -39,10 +39,6 @@
 
     
     // Return to previous page.
-    $previous = "javascript:history.go(-1)";
-    if(isset($_SERVER['HTTP_REFERER'])) {
-        $previous = $_SERVER['HTTP_REFERER'];
-    }
-    header("location:$previous");
+    header("location:../index.php");
     exit;
 ?>
